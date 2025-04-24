9b8d3735cbbeb838b495dc920b422bd0191ef47b
Anshul-Bhardwaj-21 <anshulbhardwaj201@gmail.com>
2025-04-24 12:00:19 +0530

    Create dashboard.js

---
+++ script/dashboard.js
@@ -0,0 +1,20 @@
+function goToBookingForm() {
+    window.location.href = "booking.html";
+  }
+  
+  function goToAvailability() {
+    window.location.href = "availability.html";
+  }
+  
+  function goToMyBookings() {
+    window.location.href = "my-bookings.html";
+  }
+  
+  function goToStats() {
+    window.location.href = "stats.html";
+  }
+  
+  function goToSettings() {
+    window.location.href = "settings.html";
+  }
+  
